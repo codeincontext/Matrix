c7f0b767976f4be98f3f107689e8001e33f6e292
Adam Howard <adz@skatty.me>
2012-03-03 11:08:38 +0000

    Initial commit

---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matrix::Application.config.secret_token = 'ae0786140d66daf5383e3535e519b440278544125c5136ab619e60847e2c25497db34b85fc7432f8ab66e51acf734195ecc5323932fee113356b6ec6752cef3d'
