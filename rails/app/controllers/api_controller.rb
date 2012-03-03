a930bb7e641921fb9def79f58a079c62453d00aa
Adam Howard <adz@skatty.me>
2012-03-03 15:11:36 +0000

    Add API functionality

--- rails/app/controllers/api_controller.rb
+++ rails/app/controllers/api_controller.rb
@@ -1,8 +1,22 @@
 class ApiController < ApplicationController
   def update
-    
+    success = redis.set 'matrix:values'
+    if (success)
+      render json: true, status: '200'
+    else
+      render json: false, status: '400'
+    end
   end
   def show
-    
+    values = redis.get 'matrix:values'
+    render json: values
+  end
+
+private
+  def redis
+    Redis.new
+  end
+  def redis_key
+    'matrix:values'
   end
 end
