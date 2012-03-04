f42df7d61afe5b0af4ed54adb9d2979b89146223
Adam Howard <adz@skatty.me>
2012-03-04 02:18:49 +0000

    Update rails

--- rails/app/controllers/api_controller.rb
+++ rails/app/controllers/api_controller.rb
@@ -1,21 +1,18 @@
 class ApiController < ApplicationController
   def update
-    success = redis.set 'matrix:values'
-    if (success)
-      render json: true, status: '200'
+    success = REDIS.set 'matrix:values', params[:value]
+    if success
+      render json: true, status: 200
     else
-      render json: false, status: '400'
+      render json: false, status: 400
     end
   end
   def show
-    values = redis.get 'matrix:values'
+    values = REDIS.get 'matrix:values'
     render json: values
   end
 
 private
-  def redis
-    Redis.new
-  end
   def redis_key
     'matrix:values'
   end
